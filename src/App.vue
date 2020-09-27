c2b9c049e4a196c6ebb3f1260ee7ba460fe36e4d
Daniel dos Santos Araujo <denenewton@icloud.com>
2020-09-27 03:04:39 -0300

    add

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Comentarios />
+  </div>
+</template>
+
+<script>
+import Comentarios from './components/Comentarios.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Comentarios
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+  margin-top: 60px;
+  margin-right: 30%;
+  margin-left: 30%;
+}
+</style>
---
+++ src/components/Comentarios.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="Comentarios">
+    
+    <div class="container">
+       <h1>Comentários</h1>
+       <div class="form-todo form-group">
+      
+       <p><input v-model="name" type="text" name="author" placeholder="nome" class="form-control"></p>
+     <p><textarea v-model="message"  name="message"  rows="5" placeholder="comentário" class="form-control"></textarea></p>
+      <button v-on:click="addCommets" type="submit" class="btn btn-primary">comentar</button>
+     <div class="list-group" v-for="commet in commets" :key="commet.id">
+       <div class="list-group-item mt-3">
+         <span class="comment__author">Autor: <strong>{{commet.nome}}</strong>  </span>
+         <p>{{commet.mens}}</p>
+       </div>
+
+     </div>
+    </div>
+    </div>
+    </div>   
+</template>
+     
+
+   
+
+<script>
+export default {
+    
+    data(){
+        return {
+           commets: [
+               {   
+                   id: 0,
+                   nome: 'Daniel Santos Araujo',
+                   mens: 'Gosto muito de aprender'
+               }
+
+            ],
+           name: 'daniel', 
+           message: 'fdsffsdfdsf',
+           id: 1
+                
+    
+        }                               
+           
+    },
+
+    methods: {
+               addCommets(){
+                   console.log(this.name)
+                   console.log(this.message)
+                   console.log(this.id)
+                   this.commets.push(
+                       {
+                       id: this.id ++,
+                       nome: this.name,
+                       mens: this.message
+                       }
+                   )
+
+               }
+
+
+    }      
+
+    
+    
+}
+
+</script>
+
